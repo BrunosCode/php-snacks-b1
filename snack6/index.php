444d42164f8e8ab05f5c0a33a8436e829275bc57
brunEdo <edobruno.m@gmail.com>
2021-10-20 12:51:06 +0200

    style

--- snack6/index.php
+++ snack6/index.php
@@ -37,17 +37,31 @@ function color($string){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<style>
+		.box {
+			display: inline-block;
+			width: 30%;
+			text-align: center;
+			padding: 1rem;
+		}
+		.gray {
+			background-color: gray;
+		}
+		.green {
+			background-color: green;
+		}
+	</style>
     <title>Snack 6</title>
 </head>
 <body>
     <h1>Posts</h1>
     <?php foreach($db as $role => $members) { ?>
-        <?php foreach ($members as $member) { ?>
-            <div style="background-color: <?php echo color($role) ?>"> 
+		<div class="box <?php echo color($role) ?>"> 
+			<?php foreach ($members as $member) { ?>
 
 				<h3><?= $member["name"] ?> <?= $member["lastname"] ?></h3>
-            </div>
-        <?php } ?>
+			<?php } ?>
+		</div>
     <?php } ?>
 </body>
 </html>
