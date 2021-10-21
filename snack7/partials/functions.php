27849bc45d034ee2bc7e1ecbbd593f6ba6c89310
brunEdo <edobruno.m@gmail.com>
2021-10-21 12:36:54 +0200

    snack7

---
+++ snack7/partials/functions.php
@@ -0,0 +1,27 @@
+<?php 
+function randMarks($marksLength) {
+    $marks = [];
+    for ($i = 0; $i < $marksLength; $i++){
+        array_push($marks, rand(0, 10));
+    }
+    return $marks;
+}
+
+function randName($names) {
+    return $names[rand(0, count($names) - 1)];
+}
+
+function randStudents($studentsLength, $names, $surnames, $marksLength ) {
+    $students = [];
+    for ($i = 0; $i < $studentsLength; $i++){
+        array_push($students,
+            [ 
+                "name" => randName($names), 
+                "surname" => randName($surnames),
+                "marks" => randMarks($marksLength)
+            ]
+        );
+    }
+    return $students;
+}
+?>
